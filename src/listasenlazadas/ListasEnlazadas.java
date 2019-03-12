07e14679fcb9e8f3219e9499126905b8ceb2d4e9
Jersson Mendoza <jersson926@gmail.com>
2019-03-11 23:42:20 -0500

    Class main modified

--- src/listasenlazadas/ListasEnlazadas.java
+++ src/listasenlazadas/ListasEnlazadas.java
@@ -16,27 +16,6 @@ public class ListasEnlazadas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-//        Lista l = new Lista();
-//        
-//        l.add(new Persona(0, "Juan", 20));
-//        l.add(new Persona(1, "Pepito", 10));
-//        l.add(new Persona(2, "Alex", 15));
-////        //l.remove();
-//        l.add(new Persona(3, "Kevin", 21));
-////        l.add(new Persona(4, "Brayan", 22));
-////        l.add(new Persona(5, "Britany", 23));
-//        l.addInit(new Persona(4, "Brayan", 22));
-//        l.addInit(new Persona(5, "Britany", 23));
-//        l.show();
-//        System.out.println("-----------------------------------------------------------------------------------");
-//        l.remove(2);
-//        //l.remove(0);
-//        l.show();
-//        System.out.println("Buscamdo...");
-//        l.search(4);
-//        
-//        System.out.println(l.getSize());
-        
         Controller c = new Controller();
         c.ShowMenu();
         
